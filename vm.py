716776546096b84a05c797a46c5b6c85a34191e3
T Y <uschoi77@gmail.com>
2018-08-28 10:41:48 +0900

    테스트 반환 추가

--- vm.py
+++ vm.py
@@ -7,7 +7,9 @@ class VendingMachine:
 
         tokens = raw.split(" ")
         cmd, params = tokens[0], tokens[1:]
-
+        coins = [500, 100, 50, 10]
+        moim = []
+        i = 0
         if cmd == "잔액":
             return "잔액은 " + str(self._change) + "원입니다."
 
@@ -25,7 +27,28 @@ class VendingMachine:
                 return "잔액이 부족합니다."
             self._change = self._change - price
             return beverage + "가 나왔습니다."
-
+        elif cmd == "반환":
+                self._change = params[0]
+                while self._change != 0:
+                    i = 0
+                    if self._change >= coins[i]:
+                            self._change = self._change - coins[i]
+                            return moim.append(coins[i])
+                    elif self._change >= coins[i + 1]:
+                            self._change = self._change - coins[i + 1]
+                            return moim.append(coins[i+1])
+                    elif self._change >= coins[i + 2]:
+                            self._change = self._change - coins[i + 2]
+                            return moim.append(coins[i+2])
+                    elif self._change >= coins[i + 3]:
+                            self._change = self._change - coins[i + 3]
+                            return moim.append(coins[i+3])
+                    elif self._change >= coins[i + 4]:
+                            self._change = self._change - coins[i + 4]
+                            return moim.append(coins[i+4])
+                    moim2 = ['%d원' % x for x in moim ]
+                    print ("잔액은:" + moim2 + "입니다.") 
+                return "잔액이 0원입니다."
         else:
             return "알 수 없는 명령입니다."
 
